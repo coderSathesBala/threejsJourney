eccb9cd02af31dae5426114e43720dcc2c800358
coderSathesBala <sathes352@gmail.com>
2022-02-17 14:52:49 -0500

    practiceWithGroups

--- Project5/src/script.js
+++ Project5/src/script.js
@@ -7,14 +7,50 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+group.add(cube2)
+cube2.position.x = -2
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+group.add(cube3)
+cube3.position.x = 2
+
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.z = 1
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = 1
+// mesh.position.y = -1
+// // mesh.position.z = 1
+// scene.add(mesh)
+
+// //Scale
+// // mesh.scale.x = 1.5
+
+// // mesh.rotation.y = 1
+// mesh.rotation.x = 4
+
+
 
 /**
  * Sizes
